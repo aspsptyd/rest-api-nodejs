33f2f0960ad312a1801c6feccb43a5cc06953898
aspsptyd <devadiroot@gmail.com>
2024-03-20 16:11:25 +0700

    Add: Update README

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ $ npm run dev
 ```
 
 ![Screenshot 2024-03-20 at 15 57 05](https://github.com/aspsptyd/rest-api-nodejs/assets/98740335/116995e1-23ab-46c7-b783-487661f1e4b0)
+
+```sh
+# Run build for production
+$ npm run build
+```
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,10 @@ import express, { Application, Request, Response, NextFunction } from 'express'
 const app: Application = express()
 const port: Number = 4000
 
-app.use('/health', (req: Request, res: Response, next: NextFunction) => {
+app.use('/', (req: Request, res: Response, next: NextFunction) => {
   res.status(200).send({
-    status: 200
+    code: 200,
+    message: 'Response success!'
   })
 })
 
