5c8483d0316803f091d815fc8e1ad0acd38fae7b
aspsptyd <devadiroot@gmail.com>
2024-06-08 02:42:31 +0700

    Add: menambahkan logger pada beberapa lokasi

--- src/routes/health.ts
+++ src/routes/health.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express'
 import type { Request, Response, NextFunction } from 'express'
+import { logger } from '../utils/logger'
 
 export const HealthRouter: Router = Router()
 
 HealthRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
+  logger.info('Request health data processed...')
   res.status(200).send({
-    code: 200,
-    message: 'Response success! in development'
+    status: true,
+    statusCode: 200,
+    message: 'This endpoint for Health data'
   })
 })
